c485e6fa85cf07312bf95a86f80c3ea1fe3ffc1e
rezaramadhani <rezarmd17@gmail.com>
2022-10-17 23:02:20 +0700

    firstcommit

---
+++ src/main/java/PWS_C/PWSFIrstmeet/PwsfIrstmeetApplication.java
@@ -0,0 +1,13 @@
+package PWS_C.PWSFIrstmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwsfIrstmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwsfIrstmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PWS_C/PWSFIrstmeet/PwsfIrstmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package PWS_C.PWSFIrstmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsfIrstmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
