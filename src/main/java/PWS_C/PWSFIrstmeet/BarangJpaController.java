d101f6bacc90c69a56923956916fc1d3ba80e2a6
rezaramadhani <rezarmd17@gmail.com>
2022-10-17 23:09:04 +0700

    add barang jpa

--- src/main/java/PWS_C/PWSFIrstmeet/BarangJpaController.java
+++ src/main/java/PWS_C/PWSFIrstmeet/BarangJpaController.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -27,7 +28,7 @@ public class BarangJpaController implements Serializable {
     public BarangJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("PWS_C_PWSFIrstmeet_jar_0.0.1-SNAPSHOTPU");
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
