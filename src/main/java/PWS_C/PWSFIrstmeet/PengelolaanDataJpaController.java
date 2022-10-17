2f2f325686fcd8fea1e708abbe9cc179dcd6b7fa
rezaramadhani <rezarmd17@gmail.com>
2022-10-17 23:13:09 +0700

    Add pengelolaan data jpa

--- src/main/java/PWS_C/PWSFIrstmeet/PengelolaanDataJpaController.java
+++ src/main/java/PWS_C/PWSFIrstmeet/PengelolaanDataJpaController.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -27,8 +28,8 @@ public class PengelolaanDataJpaController implements Serializable {
     public PengelolaanDataJpaController(EntityManagerFactory emf) {
         this.emf = emf;
     }
-    private EntityManagerFactory emf = null;
-
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("PWS_C_PWSFIrstmeet_jar_0.0.1-SNAPSHOTPU");
+    
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
